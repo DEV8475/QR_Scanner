f8df1c986f71f508beaaf15373d8955646c28c8a
DEV8475 <team.devinc@gmail.com>
2025-06-25 11:23:17 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -17,4 +17,4 @@ def register():
     return render_template('register.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=10000)
